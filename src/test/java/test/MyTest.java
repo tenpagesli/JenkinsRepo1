023b7a030c5124824b2942ef2b8dc91819445aa5
Ryan Li <zhe.li@canada.ca>
2019-10-07 16:17:29 -0400

    deleted 2 lines

--- src/test/java/test/MyTest.java
+++ src/test/java/test/MyTest.java
@@ -4,8 +4,6 @@ public class MyTest{
 
 	public static void main(String args) {
 		System.out.println("line1");
-		System.out.println("line2");
-		System.out.println("line3");
 		System.out.println("line4");
 	}
 
