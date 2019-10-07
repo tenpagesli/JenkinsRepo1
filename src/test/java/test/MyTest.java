14919276ebad7b539cb0cd1e615eba82369a7538
Ryan Li <zhe.li@canada.ca>
2019-10-07 16:15:00 -0400

    added print 4 lines

--- src/test/java/test/MyTest.java
+++ src/test/java/test/MyTest.java
@@ -2,6 +2,11 @@ package test;
 
 public class MyTest{
 
-
+	public static void main(String args) {
+		System.out.println("line1");
+		System.out.println("line2");
+		System.out.println("line3");
+		System.out.println("line4");
+	}
 
 }
